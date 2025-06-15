98f73abbb25126833d917febdd87e08473de4926
leety73 <interstellar0213@naver.com>
2025-06-15 12:50:07 +0900

    봇 수정

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 import praw
 
+reddit= praw.Reddit('case')
+
+subreddit = reddit.subreddit("Python")
+
+for submission in subreddit.hot(limit=1):
+    print("제목: ", submission.title)
+    print("내용: ", submission.selftext)
+    print("점수: ", submission.score)
+    print("------------\n")

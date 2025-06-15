161cc924e00ae09c575153428a304f7cb3267968
leety73 <interstellar0213@naver.com>
2025-06-15 22:59:31 +0900

    주석 처리

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-import praw
+import praw # 라이브러리는 import되지 않는다.
 
-reddit= praw.Reddit('case')
+ reddit= praw.Reddit('case' )# case 에 해당하는 설정을 사용하여 reddit API 객체 생성
 
-subreddit = reddit.subreddit("Python")
 
-for submission in subreddit.hot(limit=1):
-    print("제목: ", submission.title)
-    print("내용: ", submission.selftext)
-    print("점수: ", submission.score)
-    print("------------\n")
+ subreddit = reddit.subreddit("Python")# Python이라는 이름의 서브레딧 객체를 생성
+
+for submission in subreddit.hot(limit=1): # 해당 서브레딧에서 인기 글 1개 순회
+    print("제목: ", submission.title)# 게시글의 제목 출력
+    print("내용: ", submission.selftext)# 게시글의 내용 출력
+    print("점수: ", submission.score)# 게시글의 추천 점수 출력
+    print("------------\n")# 구분선 출력
